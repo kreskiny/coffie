c033ec79530691dbd4f28bad1262521805660ed1
eva <zykovaeva310306@gmail.com>
2024-02-28 20:46:38 +0400

    n2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QDialog
 from PyQt5.uic import loadUi
 import sqlite3
 
@@ -9,9 +9,10 @@ class CoffeeApp(QMainWindow):
         loadUi("main.ui", self)
         self.setWindowTitle("Информация о кофе")
         self.load_data()
+        self.addButton.clicked.connect(self.open_add_edit_form)
 
     def load_data(self):
-        connection = sqlite3.connect("../coffee.sqlite")
+        connection = sqlite3.connect("coffee.sqlite")
         cursor = connection.cursor()
 
         cursor.execute("SELECT * FROM Coffee")
@@ -19,25 +20,80 @@ class CoffeeApp(QMainWindow):
         coffee_data = cursor.fetchall()
 
         connection.close()
-        if coffee_data:
-            self.tableWidget.setRowCount(len(coffee_data))
-            self.tableWidget.setColumnCount(len(coffee_data[0]))
-            self.tableWidget.setHorizontalHeaderLabels(
-                ["ID", "Название сорта", "Степень обжарки", "Молотый/в зернах", "Описание вкуса", "Цена",
-                 "Объем упаковки"])
-        else:
-            print("Нет данных о кофе")
 
+        self.tableWidget.setRowCount(len(coffee_data))
+        self.tableWidget.setColumnCount(len(coffee_data[0]))
+        self.tableWidget.setHorizontalHeaderLabels(["ID", "Название сорта", "Степень обжарки", "Молотый/в зернах", "Описание вкуса", "Цена", "Объем упаковки"])
 
         for row_number, row_data in enumerate(coffee_data):
             for column_number, data in enumerate(row_data):
                 self.tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(data)))
 
+    def open_add_edit_form(self):
+        add_edit_form = AddEditCoffeeForm(edit_mode=True)
+        if add_edit_form.exec_() == QDialog.Accepted:
+            self.load_data()
+
+
+
+
+class AddEditCoffeeForm(QDialog):
+    def __init__(self, edit_mode=False):
+        super().__init__()
+        self.edit_mode = edit_mode
+        loadUi("addEditCoffeeForm.ui", self)
+        self.setWindowTitle("Add/Edit Coffee")
+
+        self.saveButton.clicked.connect(self.save_data)
+
+    def save_data(self):
+
+        # Получение значений из всех полей формы
+        fields = {
+            "sort_name": self.sortNameLineEdit.text(),
+            "roast_degree": self.roastDegreeLineEdit.text(),
+            "ground_or_whole": self.groundOrWholeLineEdit.text(),
+            "taste_description": self.tasteDescriptionLineEdit.text(),
+            "price": self.priceLineEdit.text(),
+            "package_volume": self.packageVolumeLineEdit.text()
+        }
+        print(fields)
+        connection = sqlite3.connect("coffee.sqlite")
+        cursor = connection.cursor()
+
+        try:
+            query_type = "UPDATE" if self.edit_mode else "INSERT INTO Coffee"
+        except Exception as e:
+            print(f"An error occurred while determining the query type: {e}")
+
+        print('type')
+
+        connection = sqlite3.connect("coffee.sqlite")
+        cursor = connection.cursor()
+
+        # Формирование SQL-запроса для вставки новой записи
+        query = '''INSERT INTO Coffee (sort_name, roast_degree, ground_or_whole, taste_description, price, package_volume)
+                       VALUES (?, ?, ?, ?, ?, ?)'''
+
+        # Выполнение запроса на вставку новой записи
+        cursor.execute(query, tuple(fields.values()))
+
+        # Сохранение изменений в базе данных и закрытие соединения
+        connection.commit()
+        connection.close()
+
+        # Закрытие формы
+        self.accept()
+
+
 def main():
-    app = QApplication(sys.argv)
-    window = CoffeeApp()
-    window.show()
-    sys.exit(app.exec_())
+    try:
+        app = QApplication(sys.argv)
+        window = CoffeeApp()
+        window.show()
+        sys.exit(app.exec_())
+    except Exception as e:
+        print("An error occurred:", e)
 
 if __name__ == "__main__":
     main()
